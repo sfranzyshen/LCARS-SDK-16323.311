842ca563dbe934efbeda18253d3d5fecb1824b2d
Scotty Franzyshen <sfranzyshen@gmail.com>
2019-07-05 10:41:57 -0700

    Update audio.js

--- 15088.21/interfaces/captains-chair-15130.1/module/audio.js
+++ 15088.21/interfaces/captains-chair-15130.1/module/audio.js
@@ -71,8 +71,8 @@ function audioAccessDataSuccess() {
 
 function runAudio(endPath){
     if(domlCalls.settings.values.soundEffects !== false){
-        var path = window.location.pathname;
-        path = path.substr( path, path.length - 10 );
+        var path = document.location.pathname;
+        //path = path.substr( path, path.length - 10 );
 //        var starPath = 'file://' + path;
         var starPath = path;
         var audio_buttonPress = new Audio(starPath + endPath);
